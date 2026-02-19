1fe7259f7869adee889879f55829e6f1edd61cc3
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2026-02-19 12:59:08 +0100

    Train.py

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ModelConfiguration} from './model-configuration/model-configuration';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: ModelConfiguration}
+];
---
+++ frontend/src/app/model-configuration/model-configuration.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-model-configuration',
+  imports: [
+    MatToolbar
+  ],
+  templateUrl: './model-configuration.html',
+  styleUrl: './model-configuration.css',
+})
+export class ModelConfiguration {
+
+}
