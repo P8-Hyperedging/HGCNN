001dbe36515d8b92af5b996b47d8e8635585a890
Daniel <79269401+DaniekFjaes@users.noreply.github.com>
2026-03-05 10:57:18 +0100

    aaa

--- nn/train.py
+++ nn/train.py
@@ -13,7 +13,10 @@ from data.knn_preprocessing import create_business_feature_matrix, create_label_
 from utils.utils import construct_H_with_KNN, generate_G_from_H
 from model.MoonLabHGNN.HGNN import HGNN
 from model.MoonLabHGNN.train import train_model_moonlab
+from data.data2 import load
 
+
+'''
 edge_index = torch.tensor([[0, 1, 1, 2],
                            [1, 0, 2, 1]], dtype=torch.long)
 x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
@@ -48,6 +51,34 @@ testing_range = np.arange(split+1, 2 * split)
 
 print( f"Training range: {0} - {split}, Testing range: {split+1} - {2*split}" )
 
+'''
+
+class Args:
+    def __init__(self):
+        self.data = 'cocitation'  # 'coauthorship' or 'cocitation'
+        self.dataset = 'pubmed'        # 'cora', 'dblp' for coauthorship; 'cora', 'citeseer', 'pubmed' for cocitation
+        self.split = 2             
+
+args = Args()
+
+# Load the dataset
+print(f"Loading {args.data}/{args.dataset} dataset...")
+dataset, train_idx, test_idx = load(args)
+
+# Extract data
+hypergraph = dataset['hypergraph']  # dict: {edge_id: [node_ids]}
+features = dataset['features']       # numpy array (n_nodes x n_features)
+labels = dataset['labels']           # numpy array (n_nodes x n_classes) - one-hot encoded
+n_nodes = dataset['n']
+
+print(f"Number of nodes: {n_nodes}")
+print(f"Number of hyperedges: {len(hypergraph)}")
+print(f"Feature dimension: {features.shape[1]}")
+print(f"Number of classes: {labels.shape[1]}")
+
+fm = features  # This is your feature matrix
+lv = labels.argmax(axis=1) 
+
 H = construct_H_with_KNN(fm, K_neigs=20, split_diff_scale=False, is_probH=True, m_prob=1)
 
 print(f"H shape: {H.shape}")
@@ -61,8 +92,8 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 fts = torch.Tensor(fm).to(device)
 lbls = torch.Tensor(lv).long().to(device)
 G = torch.Tensor(G).to(device)
-idx_train = torch.Tensor(training_range).long().to(device)
-idx_test = torch.Tensor(testing_range).long().to(device)
+idx_train = torch.Tensor(train_idx).long().to(device)
+idx_test = torch.Tensor(test_idx).long().to(device)
 
 n_class = int(lbls.max()) + 1
 
