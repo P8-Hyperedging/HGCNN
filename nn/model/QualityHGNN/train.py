5bff3e272a3473da5301df9b72b165007c19fc01
Daniel <79269401+DaniekFjaes@users.noreply.github.com>
2026-03-12 17:29:40 +0100

    Copied HGNN to new module
    Such that we can modify it while still using the old baseline. Presumably it makes sense to use this as a starting point

---
+++ nn/model/QualityHGNN/train.py
@@ -0,0 +1,67 @@
+
+import time
+import copy
+import torch
+
+def train_model_moonlab(model, criterion, optimizer, scheduler, num_epochs=25, print_freq=500, idx_train=None, idx_test=None, fts=None, lbls=None, G=None):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        if epoch % print_freq == 0:
+            print('-' * 10)
+            print(f'Epoch {epoch}/{num_epochs - 1}')
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            idx = idx_train if phase == 'train' else idx_test
+
+            # Iterate over data.
+            optimizer.zero_grad()
+            with torch.set_grad_enabled(phase == 'train'):
+                outputs = model(fts, G)
+                loss = criterion(outputs[idx], lbls[idx])
+                _, preds = torch.max(outputs, 1)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+            # statistics
+            running_loss += loss.item() * fts.size(0)
+            running_corrects += torch.sum(preds[idx] == lbls.data[idx])
+
+            epoch_loss = running_loss / len(idx)
+            epoch_acc = running_corrects.double() / len(idx)
+
+            if epoch % print_freq == 0:
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        if epoch % print_freq == 0:
+            print(f'Best val Acc: {best_acc:4f}')
+            print('-' * 20)
+
+    time_elapsed = time.time() - since
+    print(f'\nTraining complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f}')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
