8a5c2791e0505286c5aafee7c0e92e242368ae30
Daniel <79269401+DaniekFjaes@users.noreply.github.com>
2026-03-16 10:42:51 +0100

    Initialize weights with zero instead of random values
    This is to make it easier to tell if our changes improve or decrease accuracy

--- nn/model/QualityHGNN/layers.py
+++ nn/model/QualityHGNN/layers.py
@@ -17,10 +17,15 @@ class QHGNN_conv(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+        '''
         stdv = 1. / math.sqrt(self.weight.size(1))
         self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
+        '''
+        self.weight.data.zero_()
+        self.bias.data.zero_()
+        # We start with ones, so the model is more consistent for development.
 
     def forward(self, x: torch.Tensor, G: torch.Tensor, Q: torch.Tensor):
         x = x.matmul(self.weight) 
@@ -34,35 +39,3 @@ class QHGNN_conv(nn.Module):
         x = Q_G.matmul(x)
         return x
 
-    
-class QHGNN_fc(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(QHGNN_fc, self).__init__()
-        self.fc = nn.Linear(in_ch, out_ch)
-
-    def forward(self, x):
-        return self.fc(x)
-
-
-class QHGNN_embedding(nn.Module):
-    def __init__(self, in_ch, n_hid, dropout=0.5):
-        super(QHGNN_embedding, self).__init__()
-        self.dropout = dropout
-        self.hgc1 = QHGNN_conv(in_ch, n_hid)
-        self.hgc2 = QHGNN_conv(n_hid, n_hid)
-
-    def forward(self, x, G, Q):
-        x = F.relu(self.hgc1(x, G, Q))
-        x = F.dropout(x, self.dropout)
-        x = F.relu(self.hgc2(x, G, Q))
-        return x
-
-
-class QHGNN_classifier(nn.Module):
-    def __init__(self, n_hid, n_class):
-        super(QHGNN_classifier, self).__init__()
-        self.fc1 = nn.Linear(n_hid, n_class)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        return x
