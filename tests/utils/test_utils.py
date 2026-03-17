ee90021ab08e5ef7028bec54b0d7bdaf425be067
chris <christoffer.gamel@gmail.com>
2026-03-17 09:47:35 +0100

    test of generating G from H

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -5,7 +5,11 @@ import sys
 import numpy as np
 import pytest
 
-from utils.utils import Eu_dis
+from utils.utils import Eu_dis, _generate_G_from_H
+
+# ---------------
+# TEST Eu_dis
+# ---------------
 
 def test_eu_dis_basic():
     x = np.array([
@@ -28,3 +32,64 @@ def test_eu_dis_basic():
 
     # Matrix should be symmetric
     assert np.allclose(dist, dist.T)
+
+# ---------------
+# TEST _generate_G_from_H
+# ---------------
+
+def test_output_shape():
+    H = np.array([
+        [1, 0],
+        [1, 1],
+        [0, 1]
+    ], dtype=float)
+    G = _generate_G_from_H(H)
+    assert G.shape == (3, 3)
+
+def test_symmetric():
+    H = np.array([
+        [1, 0],
+        [1, 1],
+        [0, 1]
+    ], dtype=float)
+    G = _generate_G_from_H(H)
+    assert np.allclose(G, G.T)
+
+def test_variable_weight_reconstruction():
+    H = np.array([
+        [1, 0],
+        [1, 1],
+        [0, 1]
+    ], dtype=float)
+
+    G_direct = _generate_G_from_H(H, variable_weight=False)
+    DV2_H, W, invDE_HT_DV2 = _generate_G_from_H(H, variable_weight=True)
+    G_parts = DV2_H @ W @ invDE_HT_DV2
+
+    assert np.allclose(G_direct, G_parts)
+
+def test_single_hyperedge_two_nodes():
+    H = np.array([
+        [1],
+        [1]
+    ], dtype=float)
+
+    G = _generate_G_from_H(H)
+    expected = np.array([
+        [0.5, 0.5],
+        [0.5, 0.5]
+    ])
+    assert np.allclose(G, expected)
+
+def test_disconnected_groups():
+    H = np.array([
+        [1, 0],
+        [1, 0],
+        [0, 1],
+        [0, 1]
+    ], dtype=float)
+
+    G = _generate_G_from_H(H)
+
+    assert np.allclose(G[:2, 2:], 0)
+    assert np.allclose(G[2:, :2], 0)
