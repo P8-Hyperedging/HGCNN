a60c1ad110c6b2c18457ebc6596db34c2ec48c39
Daniel <79269401+DaniekFjaes@users.noreply.github.com>
2026-04-07 12:13:48 +0200

    minmaxxing

--- src/model/QualityHGNN/layers.py
+++ src/model/QualityHGNN/layers.py
@@ -44,8 +44,8 @@ class QHGNN_conv(nn.Module):
                 centroid = calculate_centroid_torch(feature_matrix)
                 total_distance = calculate_total_distance_to_centroid_torch(feature_matrix, centroid) + 1e-8
                 Q_updated[i, i] = (1 / total_distance) * Q[i, i] 
-                
-            Q_updated = torch.clamp(Q_updated, min=1e-6, max=1e6)
+
+            Q_updated = torch.clamp(Q_updated, min=0, max=10)
 
 
             
