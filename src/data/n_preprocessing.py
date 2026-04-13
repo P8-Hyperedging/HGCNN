Simple random splits and corrected variance max
Using https://numpy.org/doc/stable/reference/random/generated/numpy.random.permutation.html for simple splitting.

src/data/n_preprocessing.py
@@ -5,7 +5,7 @@ from data.data import OpeningHours
 import torch
 from utils.qualityutils import *
 
-MAX_POSSIBLE_VARIANCE = 6.25
+MAX_POSSIBLE_VARIANCE = 4.0 # ((1-3)^2+(5-3)^2)/2
 
 def build_hypergraph_incidence_matrix(reviews):
     """ Builds a hypergraph incidence matrix H. Rows/nodes are businesses, columns/hyperedges are users. 
@@ -126,3 +126,16 @@ def create_label_vector(businesses):
         labels[i] = round(b.stars * 2) - 2 # -2 to make class numbers 0-8 instead of 2-10
         
     return labels
+
+def rand_train_test_idx_simple(n_nodes, train_prop=0.75):
+    """ Simple random split. Train proportion is provide, the rest is validation. """
+    n = n_nodes
+    train_num = int(n * train_prop)
+    valid_num = int(n * (1-train_prop))
+    
+    perm = torch.as_tensor(np.random.permutation(n))
+    
+    train_idx = perm[:train_num]
+    valid_idx = perm[train_num:train_num + valid_num]
+    
+    return {'train': train_idx, 'valid': valid_idx}

src/model/QualityHGNN/train.py
@@ -9,7 +9,6 @@ import sys
 
 import torch
 from model.QualityHGNN.QHGNN import QHGNN
-#from sklearn.model_selection import train_test_split
 from torch import device, optim, split
 
 from data.data import *
@@ -19,7 +18,7 @@ from utils.utils import *
 
 class Train_QHGNN:
     def __init__(self):
-        self.reviews = load_postgres_review_data()
+        self.reviews = load_postgres_review_data("user_business_reviews_sw_restaurants")
 
     def train(self, num_epochs=100, 
               lr=0.009, 
@@ -66,12 +65,11 @@ class Train_QHGNN:
             print(f"  Class {label}: {count} ({count/len(lv)*100:.1f}%)")
 
         n = len(businesses)
-        split = int(n * train_proportion)
+        splits = rand_train_test_idx_simple(n, train_prop=train_proportion)
 
-        training_range = np.arange(0, split)
-        testing_range = np.arange(split, n)
-
-        print(f"Total nodes: {n}, Training nodes: {len(training_range)}, Testing nodes: {len(testing_range)}")
+        print(f"Total nodes: {n}, Training nodes: {len(splits['train'])}, Validation nodes: {len(splits['valid'])}")
+        print(f"Sample train node IDs (first 10): {splits['train'][:10].tolist()}")
+        print(f"Sample val node IDs (first 10): {splits['valid'][:10].tolist()}")
 
 
         Q =  diags(create_quality_matrix_from_H(self.reviews))
@@ -89,8 +87,8 @@ class Train_QHGNN:
         fts = torch.Tensor(fm).to(device)
         lbls = torch.Tensor(lv).long().to(device)
         G = torch.Tensor(G).to(device)
-        idx_train = torch.Tensor(training_range).long().to(device)
-        idx_test = torch.Tensor(testing_range).long().to(device)
+        idx_train = splits['train'].long().to(device)
+        idx_test = splits['valid'].long().to(device)
 
         n_class = int(lbls.max()) + 1
 